a8dc489e39982baa8b2ff74f5e02e110d9aa7329
pawar <kapil.pawar@capgemini.com>
2019-03-04 12:31:26 +0530

    modified Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,6 @@ public class Calculator
     public static void main(String[] args)
     {
         System.out.println("hello world!");
+        System.out.println("Calculator java program");
     }
 }
